31c3cad0762ac805b948ba5c4818113d584ec3ce
Eric Zorn <zornwebdev@gmail.com>
2025-01-13 13:11:46 -0500

    feat: updating error handling logic

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate dotenv;
 
 use clap::Parser;
 use dotenv::dotenv;
-use std::env;
+use std::{env, error::Error};
 
 use mixmax_csv_uploader::{
     cli::Args,
@@ -11,10 +11,10 @@ use mixmax_csv_uploader::{
     parse_people, Recipient, RecipientVariables, RequestBody,
 };
 
-#[tokio::main]
-async fn main() -> Result<(), CLIError> {
-    dotenv().ok();
-
+/// Will read in and parse the CSV,
+/// send request to MixMax and return the
+/// corresponding errors (if present)
+async fn run() -> Result<(), CLIError> {
     // Parse CLI Arguments
     let args = Args::parse();
     if !args.is_valid() {
@@ -24,17 +24,6 @@ async fn main() -> Result<(), CLIError> {
     let reader = csv::Reader::from_path(&args.file_path).map_err(|_| CLIError::FileRead)?;
     let people = parse_people(reader).map_err(|_| CLIError::ParseCSV)?;
     println!("Length of people is {}", people.len());
-
-    let body = RequestBody {
-        recipients: people
-            .iter()
-            .map(|p| Recipient {
-                email_address: p.email_address.clone(),
-                variables: RecipientVariables::from(p.clone()),
-            })
-            .collect::<Vec<Recipient>>(),
-        scheduled_at: false,
-    };
 
     // Environment variables
     let api_key = env::var("MIXMAX_API_KEY").expect("MIXMAX API Key expected to be present");
@@ -46,11 +35,33 @@ async fn main() -> Result<(), CLIError> {
 
     // Send request via HTTP to MixMax
     let sender = http::Requester::<Empty>::new(api_key, full_url);
+    let body = RequestBody {
+        recipients: people
+            .iter()
+            .map(|p| Recipient {
+                email_address: p.email_address.clone(),
+                variables: RecipientVariables::from(p.clone()),
+            })
+            .collect::<Vec<Recipient>>(),
+        scheduled_at: false,
+    };
     let res_body = sender
         .send_request(body)
         .await
         .map_err(|_| CLIError::HTTP)?;
     println!("{res_body:?}");
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok();
+
+    // Run Program
+    if let Err(e) = run().await {
+        eprintln!("{e}");
+    }
 
     Ok(())
 }
